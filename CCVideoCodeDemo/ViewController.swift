2145cbdb5a4f53747598700dbcbad5dc5876626f
Oliver Zhang <oli.zhang@gmail.com>
2017-01-13 10:33:56 +0800

    initial commit

--- CCVideoCodeDemo/ViewController.swift
+++ CCVideoCodeDemo/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UIWebViewDelegate {
+    
+    var webView: WKWebView = WKWebView()
+    
+    override func loadView() {
+        super.loadView()
+        self.view = self.webView
     }
-
+    
+    override func viewDidLoad() {
+        if let templatepath = Bundle.main.path(forResource: "demo", ofType: "html") {
+            let base = URL(string: "http://www.ftchinese.com/m/corp/demo.html")
+            do {
+                let s = try NSString(contentsOfFile:templatepath, encoding:String.Encoding.utf8.rawValue)
+                self.webView.loadHTMLString(s as String, baseURL:base)
+            } catch {
+                print ("Cannot load from local. Try load from the web! ")
+            }
+        } else {
+            print ("the html file is not found in the bundle")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
